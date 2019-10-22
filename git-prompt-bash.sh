a131400006fddacb36ce10c6239a3232f0ad17b4
Anthony Dupont <adupont@signatureresearchinc.com>
2019-10-22 09:35:37 -0400

    update user@host color to red when ssh used

--- git-prompt-bash.sh
+++ git-prompt-bash.sh
@@ -4,5 +4,12 @@
 source ~/.git-prompt.sh
 export GIT_PS1_SHOWDIRTYSTATE=1
 export GIT_PS1_SHOWCOLORHINTS=true
-PROMPT_COMMAND='__git_ps1 "\[\e[35m\]\u@\h\[\e[0m\]:\[\e[33m\]\w\[\e[0m\]" "\\\$ "'
+magenta="\[\e[35m\]"
+red="\[\e[31m\]"
+endcolor="\[\e[0m\]"
+usercolor=$magenta
+if [ -n "${SSH_CONNECTION}" ]; then
+  usercolor=$red
+fi
+PROMPT_COMMAND='__git_ps1 "${usercolor}\u@\h${endcolor}:\[\e[33m\]\w\[\e[0m\]" "\\\$ "'
 ################################################################################
