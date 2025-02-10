cd0628eeaa469c1b11ae61894f76eaf9cb273227
Anthony DuPont <tony@relinc.net>
2025-02-10 13:37:53 -0500

    add error handling for git-prompt.sh

--- git-prompt-bash.sh
+++ git-prompt-bash.sh
@@ -1,20 +1,26 @@
 ################################################################################
 # git prompt
-source ~/.git-prompt.sh
-export GIT_PS1_SHOWDIRTYSTATE=
-export GIT_PS1_SHOWSTASHSTATE=
-export GIT_PS1_SHOWUNTRACKEDFILES=
-export GIT_PS1_COMPRESSSPARSESTATE=
-export GIT_PS1_SHOWUPSTREAM=
-export GIT_PS1_SHOWCONFLICTSTATE=
-export GIT_PS1_SHOWCOLORHINTS=true
+gitpromptfile=~/.git-prompt.sh
+if [ -f ${gitpromptfile} ]; then
+	source ~/.git-prompt.sh
+	export GIT_PS1_SHOWDIRTYSTATE=
+	export GIT_PS1_SHOWSTASHSTATE=
+	export GIT_PS1_SHOWUNTRACKEDFILES=
+	export GIT_PS1_COMPRESSSPARSESTATE=
+	export GIT_PS1_SHOWUPSTREAM=
+	export GIT_PS1_SHOWCONFLICTSTATE=
+	export GIT_PS1_SHOWCOLORHINTS=true
 
-magenta="\[\e[35m\]"
-red="\[\e[31m\]"
-endcolor="\[\e[0m\]"
-usercolor=$magenta
-if [ -n "${SSH_CONNECTION}" ]; then
-  usercolor=$red
+	magenta="\[\e[35m\]"
+	red="\[\e[31m\]"
+	endcolor="\[\e[0m\]"
+	usercolor=$magenta
+	if [ -n "${SSH_CONNECTION}" ]; then
+		usercolor=$red
+	fi
+	PROMPT_COMMAND='__git_ps1 "${usercolor}\u@\h${endcolor}:\[\e[33m\]\w\[\e[0m\]" "\\\$ "'
+else
+	echo "${gitpromptfile} not found, aborting git terminal setup"
+	echo "${gitpromptfile} aborted setting up terminal with git info"
 fi
-PROMPT_COMMAND='__git_ps1 "${usercolor}\u@\h${endcolor}:\[\e[33m\]\w\[\e[0m\]" "\\\$ "'
 ################################################################################
